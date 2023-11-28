vaigu-com/otserver

commit 343f28573a352a10bf4a85abee759a1d1c314ec0

fix: some scripts (#1821)
Continues fixing of #1784 and #1847 

• Improved message on exercise_training.
• Fixes reward charges.
• Created flag in prison bosses' scripts to enable minus players and removed already created functions.
• Removed create torch when player login.
• Fixes in 'clearBossRoom' function usage on bosses teleports.
• Fixed count days and time on ferumbras_lever and fixed mechanic of Ascending Ferumbras boss.
• Fixed xp boost storage resetter (on very ss, the storage was being presented, and the player always buy it with 30 tc).
• Added 13.21 new mounts, reordered and fixed typos, changed the word moveable to movable.
• Removed unnecessary spaces.

data/XML/mounts.xml
@@ -216,5 +216,9 @@
 	<mount id="214" clientid="1673" name="Surly Steer" speed="10" premium="no" type="store" />
 	<mount id="215" clientid="1674" name="Obstinate Ox" speed="10" premium="no" type="store" />
 	<mount id="216" clientid="1677" name="Darkfire Devourer" speed="10" premium="no" type="store" />
-	<mount id="217" clientid="1682" name="Spirit of Purity" speed="10" premium="yes" type="quest" />
+	<mount id="217" clientid="1682" name="Spirit of Purity" speed="10" premium="no" type="store" />
+	<mount id="218" clientid="1632" name="Foxmouse" speed="10" premium="no" type="store" />
+	<mount id="219" clientid="1685" name="Doom Skull" speed="10" premium="no" type="store" />
+	<mount id="220" clientid="1686" name="Magma Skull" speed="10" premium="no" type="store" />
+	<mount id="221" clientid="1687" name="Corpsefire Skull" speed="10" premium="no" type="store" />
 </mounts>

data/modules/scripts/gamestore/gamestore.lua
@@ -1115,19 +1115,19 @@ GameStore.Categories = {
 		state = GameStore.States.STATE_NONE,
 		offers = {
 			{
-				icons = { "Arctic_Unicorn.png" },
-				name = "Artic Unicorn",
-				price = 870,
-				id = 114,
-				description = "{character}\n{speedboost}\n\n<i>The Arctic Unicorn lives in a deep rivalry with its cousin the Blazing Unicorn. Even though they were born in completely different areas, they somehow share the same bloodline. The eternal battle between fire and ice continues. Who will win? Tangerine vs.crystal blue! The choice is yours!</i>",
-				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
-			},
-			{
 				icons = { "Armoured_War_Horse.png" },
 				name = "Armoured War Horse",
 				price = 870,
 				id = 23,
 				description = "{character}\n{speedboost}\n\n<i>The Armoured War Horse is a dangerous black beauty! When you see its threatening, blood-red eyes coming towards you, you'll know trouble is on its way. Protected by its heavy armour plates, the warhorse is the perfect partner for dangerous hunting sessions and excessive enemy slaughtering.</i>",
+				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
+			},
+			{
+				icons = { "Arctic_Unicorn.png" },
+				name = "Artic Unicorn",
+				price = 870,
+				id = 114,
+				description = "{character}\n{speedboost}\n\n<i>The Arctic Unicorn lives in a deep rivalry with its cousin the Blazing Unicorn. Even though they were born in completely different areas, they somehow share the same bloodline. The eternal battle between fire and ice continues. Who will win? Tangerine vs.crystal blue! The choice is yours!</i>",
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 			},
 			{
@@ -1179,6 +1179,14 @@ GameStore.Categories = {
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 			},
 			{
+				icons = { "Bogwurm.png" },
+				name = "Bogwurm",
+				price = 870,
+				id = 189,
+				description = "{character}\n{speedboost}\n\n<i>The Bogwurm, Gloomwurm, and Rustwurm belong to a little known subset of the dragon family, and usually live out their lives in habitats far away from human interaction. Them being cunning hunters, and their keen sense of perception make these wurms great companions for whomever can locate and tame them.</i>",
+				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
+			},
+			{
 				icons = { "Boreal_Owl.png" },
 				name = "Boreal Owl",
 				price = 870,
@@ -1192,14 +1200,6 @@ GameStore.Categories = {
 				price = 750,
 				id = 208,
 				description = "{character}\n{speedboost}\n\n<i>The vibrant colours and elegance of a Tangerine Flecked Koi, a Brass Speckled Koi, and an Ink Spotted Koi make them a fascinating sight to behold, and their boisterous nature and speed will make you the first to arrive wherever there are riches to gain. Wield your weapon as gracefully and effortlessly as your swimming companion glides through the water, and the two of you will make the perfect and most deadly pair.</i>",
-				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
-			},
-			{
-				icons = { "Bogwurm.png" },
-				name = "Bogwurm",
-				price = 870,
-				id = 189,
-				description = "{character}\n{speedboost}\n\n<i>The Bogwurm, Gloomwurm, and Rustwurm belong to a little known subset of the dragon family, and usually live out their lives in habitats far away from human interaction. Them being cunning hunters, and their keen sense of perception make these wurms great companions for whomever can locate and tame them.</i>",
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 			},
 			{
@@ -1275,6 +1275,16 @@ GameStore.Categories = {
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 			},
 			{
+				icons = { "Corpsefire_Skull.png" },
+				name = "Corpsefire Skull",
+				price = 750,
+				id = 220,
+				description = "{character}\n{speedboost}\n\n<i>Skulls are the infernal heralds of untamed power. Bodies are obsolete when sinister forces animate your being. Embrace their presence and command the devastating might that awaits on the back of an eerie Corpsefire Skull!</i>",
+				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
+				state = GameStore.States.STATE_NEW,
+				home = true,
+			},
+			{
 				icons = { "Cranium_Spider.png" },
 				name = "Cranium Spider",
 				price = 690,
@@ -1307,6 +1317,14 @@ GameStore.Categories = {
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 			},
 			{
+				icons = { "Darkfire_Devourer.png" },
+				name = "Darkfire Devourer",
+				price = 1300,
+				id = 213,
+				description = "{character}\n{speedboost}\n\n<i>A wild, ancient creature, which had been hiding in the depths of the shadows for a very long time, has been spotted in Tibia again! The almighty Shadow Draptor has returned and only the bravest Tibians can control such a beast!</i>",
+				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
+			},
+			{
 				icons = { "Dawn_Strayer.png" },
 				name = "Dawn Strayer",
 				price = 870,
@@ -1329,6 +1347,16 @@ GameStore.Categories = {
 				id = 41,
 				description = "{character}\n{speedboost}\n\n<i>Its roaring is piercing marrow and bone and can be heard over ten miles away. The Desert King is the undisputed ruler of its territory and no one messes with this animal. Show no fear and prove yourself worthy of its trust and you will get yourself a valuable companion for your adventures.</i>",
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
+			},
+			{
+				icons = { "Doom_Skull.png" },
+				name = "Doom Skull",
+				price = 750,
+				id = 219,
+				description = "{character}\n{speedboost}\n\n<i>Skulls are the infernal heralds of untamed power. Bodies are obsolete when sinister forces animate your being. Embrace their presence and command the devastating might that awaits on the back of a grim Doom Skull.</i>",
+				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
+				state = GameStore.States.STATE_NEW,
+				home = true,
 			},
 			{
 				icons = { "Doombringer.png" },
@@ -1496,6 +1524,14 @@ GameStore.Categories = {
 				price = 900,
 				id = 65,
 				description = "{character}\n{speedboost}\n\n<i>The Flying Divan is the perfect mount for those who are too busy to take care of an animal mount or simply like to travel on a beautiful, magic hand-woven carpet.</i>",
+				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
+			},
+			{
+				icons = { "Foxmouse.png" },
+				name = "Foxmouse",
+				price = 750,
+				id = 218,
+				description = "{character}\n{speedboost}\n\n<i>A wild, ancient creature, which had been hiding in the depths of the shadows for a very long time, has been spotted in Tibia again! The almighty Shadow Draptor has returned and only the bravest Tibians can control such a beast!</i>",
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 			},
 			{
@@ -1697,6 +1733,16 @@ GameStore.Categories = {
 				id = 66,
 				description = "{character}\n{speedboost}\n\n<i>The Magic Carpet is the perfect mount for those who are too busy to take care of an animal mount or simply like to travel on a beautiful, magic hand-woven carpet.</i>",
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
+			},
+			{
+				icons = { "Magma_Skull.png" },
+				name = "Magma Skull",
+				price = 750,
+				id = 220,
+				description = "{character}\n{speedboost}\n\n<i>Skulls are the infernal heralds of untamed power. Bodies are obsolete when sinister forces animate your being. Embrace their presence and command the devastating might that awaits on the back of a blazing Magma Skull.</i>",
+				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
+				state = GameStore.States.STATE_NEW,
+				home = true,
 			},
 			{
 				icons = { "Marsh_Toad.png" },
@@ -2027,6 +2073,14 @@ GameStore.Categories = {
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 			},
 			{
+				icons = { "Snow_Strider.png" },
+				name = "Snow Strider",
+				price = 870,
+				id = 164,
+				description = "{character}\n{speedboost}\n\n<i>A magical fire burns inside these wolves. Bred as the faithful guardians for an eccentric wizard's tower, these creatures make for loyal companions during your travels. While not originally intended for riding, their sturdy frame makes the Dawn Strayer, Dusk Pryer and Snow Strider suitable mounts.</i>",
+				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
+			},
+			{
 				icons = { "Snowy_Owl.png" },
 				name = "Snowy Owl",
 				price = 870,
@@ -2035,11 +2089,11 @@ GameStore.Categories = {
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 			},
 			{
-				icons = { "Snow_Strider.png" },
-				name = "Snow Strider",
-				price = 870,
-				id = 164,
-				description = "{character}\n{speedboost}\n\n<i>A magical fire burns inside these wolves. Bred as the faithful guardians for an eccentric wizard's tower, these creatures make for loyal companions during your travels. While not originally intended for riding, their sturdy frame makes the Dawn Strayer, Dusk Pryer and Snow Strider suitable mounts.</i>",
+				icons = { "Spirit_of_Purity.png" },
+				name = "Spirit of Purity",
+				price = 1000,
+				id = 215,
+				description = "{character}\n{speedboost}\n\n<i>A wild, ancient creature, which had been hiding in the depths of the shadows for a very long time, has been spotted in Tibia again! The almighty Shadow Draptor has returned and only the bravest Tibians can control such a beast!</i>",
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 			},
 			{

data/modules/scripts/gamestore/init.lua
@@ -438,7 +438,7 @@ function parseBuyStoreOffer(playerId, msg)
 	-- Handled errors have a code index and unhandled errors do not
 	local pcallOk, pcallError = pcall(function()
 		if offer.type == GameStore.OfferTypes.OFFER_TYPE_ITEM then
-			GameStore.processItemPurchase(player, offer.itemtype, offer.count, offer.moveable)
+			GameStore.processItemPurchase(player, offer.itemtype, offer.count, offer.movable)
 		elseif offer.type == GameStore.OfferTypes.OFFER_TYPE_POUCH then
 			GameStore.processItemPurchase(player, offer.itemtype, offer.count)
 		elseif offer.type == GameStore.OfferTypes.OFFER_TYPE_INSTANT_REWARD_ACCESS then
@@ -446,13 +446,13 @@ function parseBuyStoreOffer(playerId, msg)
 		elseif offer.type == GameStore.OfferTypes.OFFER_TYPE_CHARMS then
 			GameStore.processCharmsPurchase(player)
 		elseif offer.type == GameStore.OfferTypes.OFFER_TYPE_BLESSINGS then
-			GameStore.processSignleBlessingPurchase(player, offer.blessid, offer.count)
+			GameStore.processSingleBlessingPurchase(player, offer.blessid, offer.count)
 		elseif offer.type == GameStore.OfferTypes.OFFER_TYPE_ALLBLESSINGS then
 			GameStore.processAllBlessingsPurchase(player, offer.count)
 		elseif offer.type == GameStore.OfferTypes.OFFER_TYPE_PREMIUM then
 			GameStore.processPremiumPurchase(player, offer.id)
 		elseif offer.type == GameStore.OfferTypes.OFFER_TYPE_STACKABLE then
-			GameStore.processStackablePurchase(player, offer.itemtype, offer.count, offer.name, offer.moveable)
+			GameStore.processStackablePurchase(player, offer.itemtype, offer.count, offer.name, offer.movable)
 		elseif offer.type == GameStore.OfferTypes.OFFER_TYPE_HOUSE then
 			GameStore.processHouseRelatedPurchase(player, offer)
 		elseif offer.type == GameStore.OfferTypes.OFFER_TYPE_OUTFIT then
@@ -477,7 +477,7 @@ function parseBuyStoreOffer(playerId, msg)
 		elseif offer.type == GameStore.OfferTypes.OFFER_TYPE_TEMPLE then
 			GameStore.processTempleTeleportPurchase(player)
 		elseif offer.type == GameStore.OfferTypes.OFFER_TYPE_CHARGES then
-			GameStore.processChargesPurchase(player, offer.itemtype, offer.name, offer.charges, offer.moveable)
+			GameStore.processChargesPurchase(player, offer.itemtype, offer.name, offer.charges, offer.movable)
 		elseif offer.type == GameStore.OfferTypes.OFFER_TYPE_HIRELING then
 			local hirelingName = msg:getString()
 			local sex = msg:getByte()
@@ -1495,7 +1495,7 @@ end
 -- take a table {code = ..., message = ...} if the error is handled. When no code
 -- index is present the error is assumed to be unhandled.
 
-function GameStore.processItemPurchase(player, offerId, offerCount, moveable)
+function GameStore.processItemPurchase(player, offerId, offerCount, movable)
 	if player:getFreeCapacity() < ItemType(offerId):getWeight(offerCount) then
 		return error({ code = 0, message = "Please make sure you have free capacity to hold this item." })
 	end
@@ -1504,7 +1504,7 @@ function GameStore.processItemPurchase(player, offerId, offerCount, moveable)
 	if inbox then
 		for t = 1, offerCount do
 			local inboxItem = inbox:addItem(offerId, offerCount or 1)
-			if moveable ~= true and inboxItem then
+			if movable ~= true and inboxItem then
 				inboxItem:setAttribute(ITEM_ATTRIBUTE_STORE, systemTime())
 			end
 		end
@@ -1513,7 +1513,7 @@ function GameStore.processItemPurchase(player, offerId, offerCount, moveable)
 	end
 end
 
-function GameStore.processChargesPurchase(player, itemtype, name, charges, moveable)
+function GameStore.processChargesPurchase(player, itemtype, name, charges, movable)
 	if player:getFreeCapacity() < ItemType(itemtype):getWeight(1) then
 		return error({ code = 0, message = "Please make sure you have free capacity to hold this item." })
 	end
@@ -1522,7 +1522,7 @@ function GameStore.processChargesPurchase(player, itemtype, name, charges, movea
 	if inbox then
 		local inboxItem = inbox:addItem(itemtype, charges)
 
-		if moveable ~= true and inboxItem then
+		if movable ~= true and inboxItem then
 			inboxItem:setAttribute(ITEM_ATTRIBUTE_STORE, systemTime())
 		end
 	else
@@ -1530,7 +1530,7 @@ function GameStore.processChargesPurchase(player, itemtype, name, charges, movea
 	end
 end
 
-function GameStore.processSignleBlessingPurchase(player, blessId, count)
+function GameStore.processSingleBlessingPurchase(player, blessId, count)
 	player:addBlessing(blessId, count)
 end
 
@@ -1563,7 +1563,7 @@ function GameStore.processPremiumPurchase(player, offerId)
 	end
 end
 
-function GameStore.processStackablePurchase(player, offerId, offerCount, offerName, moveable)
+function GameStore.processStackablePurchase(player, offerId, offerCount, offerName, movable)
 	local function isKegItem(itemId)
 		return itemId >= ITEM_KEG_START and itemId <= ITEM_KEG_END
 	end
@@ -1599,12 +1599,12 @@ function GameStore.processStackablePurchase(player, offerId, offerCount, offerNa
 						local kegItem = parcel:addItem(offerId, 1)
 						kegItem:setAttribute(ITEM_ATTRIBUTE_CHARGES, pack)
 
-						if moveable ~= true and kegItem then
+						if movable ~= true and kegItem then
 							kegItem:setAttribute(ITEM_ATTRIBUTE_STORE, systemTime())
 						end
 					else
 						local parcelItem = parcel:addItem(offerId, pack)
-						if moveable ~= true and parcelItem then
+						if movable ~= true and parcelItem then
 							parcelItem:setAttribute(ITEM_ATTRIBUTE_STORE, systemTime())
 						end
 					end
@@ -1613,7 +1613,7 @@ function GameStore.processStackablePurchase(player, offerId, offerCount, offerNa
 			end
 		else
 			local item = inbox:addItem(offerId, isKeg and 1 or offerCount)
-			if moveable ~= true and item then
+			if movable ~= true and item then
 				item:setAttribute(ITEM_ATTRIBUTE_STORE, systemTime())
 			end
 			if item and isKeg then
@@ -1645,7 +1645,7 @@ function GameStore.processHouseRelatedPurchase(player, offer)
 					decoKit:setAttribute(ITEM_ATTRIBUTE_DATE, offer.count)
 				end
 
-				if offer.moveable ~= true then
+				if offer.movable ~= true then
 					decoKit:setAttribute(ITEM_ATTRIBUTE_STORE, systemTime())
 				end
 			end

config.lua.dist
@@ -165,7 +165,6 @@ pvpRateDamageTakenPerLevel = 0.0 -- recommended to start with 0.1
 pvpRateDamageReductionPerLevel = 0.0 -- recommended to start with 0.1
 pvpMaxLevelDifference = 0
 
-
 -- Wheel of destiny system
 -- NOTE: set wheelSystemEnabled = false to disable the wheel of destiny
 -- NOTE: only the wheel points are modified, all other data is on the client executable and cannot be modified

data-canary/scripts/globalevents/global_server_save.lua
@@ -10,8 +10,6 @@ local function ServerSave()
 	end
 	-- Updating daily reward next server save
 	UpdateDailyRewardGlobalStorage(DailyReward.storages.lastServerSave, os.time())
-	-- Reset gamestore exp boost count.
-	db.query("UPDATE `player_storage` SET `value` = 0 WHERE `player_storage`.`key` = 51052")
 end
 
 local function ServerSaveWarning(time)

data-otservbr-global/monster/quests/ferumbras_ascendant/bosses/ferumbras_soul_splinter.lua
@@ -38,7 +38,7 @@ monster.flags = {
 	hostile = true,
 	convinceable = false,
 	pushable = false,
-	rewardBoss = true,
+	rewardBoss = false,
 	illusionable = false,
 	canPushItems = true,
 	canPushCreatures = true,
@@ -53,7 +53,7 @@ monster.flags = {
 }
 
 monster.events = {
-	"FerumbrasSoulSplinter",
+	"FerumbrasSoulSplinterDeath",
 }
 
 monster.light = {

data-otservbr-global/monster/quests/ferumbras_ascendant/ferumbras_essence.lua
@@ -1,7 +1,7 @@
 local mType = Game.createMonsterType("Ferumbras Essence")
 local monster = {}
 
-monster.description = " ferumbras essence"
+monster.description = "ferumbras essence"
 monster.experience = 0
 monster.outfit = {
 	lookType = 294,
@@ -13,8 +13,12 @@ monster.outfit = {
 	lookMount = 0,
 }
 
-monster.health = 115
-monster.maxHealth = 115
+monster.events = {
+	"FerumbrasEssenceImmortal",
+}
+
+monster.health = 35000
+monster.maxHealth = 35000
 monster.race = "undead"
 monster.corpse = 9591
 monster.speed = 100
@@ -38,7 +42,7 @@ monster.flags = {
 	hostile = true,
 	convinceable = false,
 	pushable = false,
-	rewardBoss = true,
+	rewardBoss = false,
 	illusionable = false,
 	canPushItems = true,
 	canPushCreatures = false,

data-otservbr-global/scripts/actions/other/enchanting.lua
@@ -66,7 +66,7 @@ function enchanting.onUse(player, item, fromPosition, target, toPosition, isHotk
 		return true
 	end
 
-	if item.itemid == 676 and table.contains({ 9020 }, target.itemid) then
+	if item.itemid == 676 and target.itemid == 9020 then
 		target:transform(9019)
 		item:remove(1)
 		toPosition:sendMagicEffect(CONST_ME_MAGIC_RED)

commit 8a99b0387f274da781fe00eb016671b8756b2f26

fix: invalid mount offer from store (#1933)
Fixes bug introduced from #1821 

A mount was added that does not yet exist in appearances, until we add a new dat, we will just ignore their existence.
Removed invalid mounts.

data/XML/mounts.xml
@@ -218,7 +218,7 @@
 	<mount id="216" clientid="1677" name="Darkfire Devourer" speed="10" premium="no" type="store" />
 	<mount id="217" clientid="1682" name="Spirit of Purity" speed="10" premium="no" type="store" />
 	<mount id="218" clientid="1632" name="Foxmouse" speed="10" premium="no" type="store" />
-	<mount id="219" clientid="1685" name="Doom Skull" speed="10" premium="no" type="store" />
+	<!--<mount id="219" clientid="1685" name="Doom Skull" speed="10" premium="no" type="store" />
 	<mount id="220" clientid="1686" name="Magma Skull" speed="10" premium="no" type="store" />
-	<mount id="221" clientid="1687" name="Corpsefire Skull" speed="10" premium="no" type="store" />
+	<mount id="221" clientid="1687" name="Corpsefire Skull" speed="10" premium="no" type="store"-->
 </mounts>

data/modules/scripts/gamestore/gamestore.lua
@@ -1274,16 +1274,16 @@ GameStore.Categories = {
 				description = "{character}\n{speedboost}\n\n<i>If the Coralripper moves its fins, it generates enough air pressure that it can even float over land. Its numerous eyes allow it to quickly detect dangers even in confusing situations and eliminate them with one powerful bite. If you watch your fingers, you are going to be good friends.</i>",
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 			},
-			{
+			--[[{
 				icons = { "Corpsefire_Skull.png" },
 				name = "Corpsefire Skull",
 				price = 750,
-				id = 220,
+				id = 221,
 				description = "{character}\n{speedboost}\n\n<i>Skulls are the infernal heralds of untamed power. Bodies are obsolete when sinister forces animate your being. Embrace their presence and command the devastating might that awaits on the back of an eerie Corpsefire Skull!</i>",
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 				state = GameStore.States.STATE_NEW,
 				home = true,
-			},
+			},]]
 			{
 				icons = { "Cranium_Spider.png" },
 				name = "Cranium Spider",
@@ -1348,7 +1348,7 @@ GameStore.Categories = {
 				description = "{character}\n{speedboost}\n\n<i>Its roaring is piercing marrow and bone and can be heard over ten miles away. The Desert King is the undisputed ruler of its territory and no one messes with this animal. Show no fear and prove yourself worthy of its trust and you will get yourself a valuable companion for your adventures.</i>",
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 			},
-			{
+			--[[{
 				icons = { "Doom_Skull.png" },
 				name = "Doom Skull",
 				price = 750,
@@ -1357,7 +1357,7 @@ GameStore.Categories = {
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 				state = GameStore.States.STATE_NEW,
 				home = true,
-			},
+			},]]
 			{
 				icons = { "Doombringer.png" },
 				name = "Doombringer",
@@ -1734,7 +1734,7 @@ GameStore.Categories = {
 				description = "{character}\n{speedboost}\n\n<i>The Magic Carpet is the perfect mount for those who are too busy to take care of an animal mount or simply like to travel on a beautiful, magic hand-woven carpet.</i>",
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 			},
-			{
+			--[[{
 				icons = { "Magma_Skull.png" },
 				name = "Magma Skull",
 				price = 750,
@@ -1743,7 +1743,7 @@ GameStore.Categories = {
 				type = GameStore.OfferTypes.OFFER_TYPE_MOUNT,
 				state = GameStore.States.STATE_NEW,
 				home = true,
-			},
+			},]]
 			{
 				icons = { "Marsh_Toad.png" },
 				name = "Marsh Toad",

data/modules/scripts/gamestore/init.lua
@@ -2137,7 +2137,12 @@ function sendHomePage(playerId)
 			msg:addString(offer.icons[1])
 		elseif type == GameStore.ConverType.SHOW_MOUNT then
 			local mount = Mount(offer.id)
-			msg:addU16(mount:getClientId())
+			if mount then
+				msg:addU16(mount:getClientId())
+			else
+				logger.debug("[sendHomePage] mount with id {} not exist, ignoring to avoid a debug on the client", offer.id)
+				msg:addU16(0)
+			end
 		elseif type == GameStore.ConverType.SHOW_ITEM then
 			msg:addU16(offer.itemtype)
 		elseif type == GameStore.ConverType.SHOW_OUTFIT then

src/creatures/appearance/mounts/mounts.cpp
@@ -31,7 +31,7 @@ bool Mounts::loadFromXml() {
 	for (auto mountNode : doc.child("mounts").children()) {
 		uint16_t lookType = pugi::cast<uint16_t>(mountNode.attribute("clientid").value());
 		if (g_configManager().getBoolean(WARN_UNSAFE_SCRIPTS, __FUNCTION__) && lookType != 0 && !g_game().isLookTypeRegistered(lookType)) {
-			g_logger().warn("{} - An unregistered creature looktype type with id '{}' was blocked to prevent client crash.", __FUNCTION__, lookType);
+			g_logger().warn("{} - An unregistered creature mount with id '{}' was blocked to prevent client crash.", __FUNCTION__, lookType);
 			continue;
 		}
 

src/lua/functions/lua_functions_loader.cpp
@@ -373,7 +373,8 @@ std::string LuaFunctionsLoader::getFormatedLoggerMessage(lua_State* L) {
 	try {
 		return fmt::vformat(format, args);
 	} catch (const fmt::format_error &e) {
-		g_logger().error("[{}] format error: {}", __FUNCTION__, e.what());
+		g_logger().debug("[{}] format error: {}", __FUNCTION__, e.what());
+		reportErrorFunc(fmt::format("Format error, {}", e.what()));
 	}
 
 	return {};