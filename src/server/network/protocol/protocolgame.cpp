40404d82aa50f45055ab69c4790b5de206018209
un000000 <101636937+un000000@users.noreply.github.com>
2025-04-14 16:22:13 +0200

    fix: use correct table for npc display name translation

--- src/server/network/protocol/protocolgame.cpp
+++ src/server/network/protocol/protocolgame.cpp
@@ -7840,7 +7840,7 @@ void ProtocolGame::AddCreature(NetworkMessage &msg, const std::shared_ptr<Creatu
 		if (!oldProtocol && creature->isHealthHidden()) {
 			msg.addString(""); // ProtocolGame::AddCreature - empty
 		} else {
-			const std::string creatureName = TryTranslate(creature->getName(), "npc_names", player);
+			const std::string creatureName = TryTranslate(creature->getName(), "npc_name", player);
 			msg.addString(creatureName); // ProtocolGame::AddCreature - creature->getName()
 		}
 	}
