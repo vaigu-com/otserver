073c9e0585ae7dbd99cf391599bf04161e814c05
un000000 <101636937+un000000@users.noreply.github.com>
2025-04-15 17:16:47 +0200

    improve: increase midnight panther head droprate for quest

--- data-vaigu/monster/magicals/midnight_panther.lua
+++ data-vaigu/monster/magicals/midnight_panther.lua
@@ -81,7 +81,7 @@ monster.loot = {
 	{ name = "gold coin", chance = 100000, maxCount = 57 },
 	{ id = 3052, chance = 12500 }, -- life ring
 	{ name = "meat", chance = 25000, maxCount = 4 },
-	{ name = "panther head", chance = 12500 },
+	{ name = "panther head", chance = 100000 },
 	{ name = "panther paw", chance = 100000 },
 }
 
