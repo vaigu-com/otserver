f2909df2eb4513b4c9e0291505962dae91301625
un000000 <101636937+un000000@users.noreply.github.com>
2025-04-19 12:43:40 +0200

    improve: weaken magic shield

--- data/scripts/spells/support/magic_shield.lua
+++ data/scripts/spells/support/magic_shield.lua
@@ -9,7 +9,8 @@ function spell.onCastSpell(creature, var)
 	condition:setParameter(CONDITION_PARAM_TICKS, 180000)
 	local player = creature:getPlayer()
 	local grade = player:upgradeSpellsWOD("Magic Shield")
-	local shield = 300 + 7.6 * player:getLevel() + 7 * player:getMagicLevel()
+	-- Vaigu custom
+	local shield = 200 + 3.5 * player:getLevel() + 3.5 * player:getMagicLevel()
 	if grade >= WHEEL_GRADE_REGULAR then
 		shield = shield * 1.25
 	end
