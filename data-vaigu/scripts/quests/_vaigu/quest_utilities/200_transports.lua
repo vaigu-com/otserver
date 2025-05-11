1b190a556da6f4e96221bcdecd91d46c555990cf
un000000 <101636937+un000000@users.noreply.github.com>
2025-05-12 00:09:36 +0200

    feature: add tarzan ship dock at mundral village

--- data-vaigu/scripts/quests/_vaigu/quest_utilities/200_transports.lua
+++ data-vaigu/scripts/quests/_vaigu/quest_utilities/200_transports.lua
@@ -79,6 +79,7 @@ pseudoQuest
 				{ toPos = Position(7038, 837, 7), name = "Kongo" },
 				{ toPos = Position(6563, 701, 7), name = "Ornuld Camp" },
 				{ toPos = Position(6730, 705, 7), name = "Heart of the Jungle" },
+				{ toPos = Position(6879, 743, 7), name = "Mundral Daycare"}
 			},
 			FISHERMAN_SHIP = {
 				{ toPos = Position(5801, 1649, 7), name = "Mirkotown Slums" },
