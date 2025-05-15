39af19643455b6fb0a237748bf44aa47102bcdb4
unknown <denisdenis666@gmail.com>
2025-05-15 12:15:43 +0200

    add_custom_task_bosses

---
+++ data-vaigu/monster/_task_boss_custom/boreth.lua
@@ -0,0 +1,118 @@
+local mType = Game.createMonsterType("Boreth")
+local monster = {}
+
+monster.description = "boreth"
+monster.experience = 1800
+monster.outfit = {
+	lookType = 287,
+	lookHead = 0,
+	lookBody = 0,
+	lookLegs = 0,
+	lookFeet = 0,
+	lookAddons = 0,
+	lookMount = 0,
+}
+
+monster.bosstiary = {
+	bossRaceId = 479,
+	bossRace = RARITY_BANE,
+
+}
+monster.health = 1400
+monster.maxHealth = 1400
+monster.race = "undead"
+monster.corpse = 8109
+monster.speed = 185
+monster.manaCost = 0
+
+monster.changeTarget = {
+	interval = 5000,
+	chance = 10,
+}
+
+monster.strategiesTarget = {
+	nearest = 70,
+	health = 10,
+	damage = 10,
+	random = 10,
+}
+
+monster.flags = {
+	summonable = false,
+	attackable = true,
+	hostile = true,
+	convinceable = false,
+	pushable = false,
+	rewardBoss = false,
+	illusionable = false,
+	canPushItems = true,
+	canPushCreatures = true,
+	staticAttackChance = 90,
+	targetDistance = 1,
+	runHealth = 0,
+	healthHidden = false,
+	isBlockable = false,
+	canWalkOnEnergy = true,
+	canWalkOnFire = true,
+	canWalkOnPoison = true,
+}
+
+monster.light = {
+	level = 0,
+	color = 0,
+}
+
+monster.summon = {
+	maxSummons = 3,
+	summons = {
+		{ name = "Banshee", chance = 50, interval = 4000, count = 1 },
+		{ name = "Vampire", chance = 50, interval = 2000, count = 1 },
+	},
+}
+
+monster.voices = {
+	interval = 5000,
+	chance = 10,
+}
+
+monster.loot = {
+	{ id = 3043, chance = 20000, maxCount = 2 },
+	{ id = 22516, chance = 15000 },
+	{ id = 8045, chance = 12000 },
+	{ id = 3434, chance = 35000 },
+}
+
+monster.attacks = {
+	{ name = "melee", interval = 2000, chance = 100, minDamage = -180, maxDamage = -235 },
+	{ name = "combat", interval = 1000, chance = 9, type = COMBAT_LIFEDRAIN, minDamage = 0, maxDamage = -300, radius = 4, effect = CONST_ME_MAGIC_RED, target = false },
+}
+
+monster.defenses = {
+	defense = 30,
+	armor = 30,
+	--	mitigation = ???,
+	{ name = "combat", interval = 1000, chance = 25, type = COMBAT_HEALING, minDamage = 100, maxDamage = 195, effect = CONST_ME_MAGIC_BLUE, target = false },
+	{ name = "invisible", interval = 3000, chance = 30, effect = CONST_ME_MAGIC_BLUE },
+}
+
+monster.elements = {
+	{ type = COMBAT_PHYSICALDAMAGE, percent = 60 },
+	{ type = COMBAT_ENERGYDAMAGE, percent = 0 },
+	{ type = COMBAT_EARTHDAMAGE, percent = 100 },
+	{ type = COMBAT_FIREDAMAGE, percent = 0 },
+	{ type = COMBAT_LIFEDRAIN, percent = 100 },
+	{ type = COMBAT_MANADRAIN, percent = 0 },
+	{ type = COMBAT_DROWNDAMAGE, percent = 0 },
+	{ type = COMBAT_ICEDAMAGE, percent = 0 },
+	{ type = COMBAT_HOLYDAMAGE, percent = -1 },
+	{ type = COMBAT_DEATHDAMAGE, percent = 100 },
+}
+
+monster.immunities = {
+	{ type = "paralyze", condition = true },
+	{ type = "outfit", condition = false },
+	{ type = "invisible", condition = true },
+	{ type = "bleed", condition = false },
+}
+
+mType:register(monster)
---
+++ data-vaigu/monster/_task_boss_custom/falconius_grandus.lua
@@ -0,0 +1,123 @@
+local mType = Game.createMonsterType("Falconius Grandus")
+local monster = {}
+
+monster.description = "a falconius grandus"
+monster.experience = 20000
+monster.outfit = {
+	lookType = 1071,
+	lookHead = 57,
+	lookBody = 96,
+	lookLegs = 76,
+	lookFeet = 105,
+	lookAddons = 2,
+	lookMount = 0,
+}
+
+monster.health = 15000
+monster.maxHealth = 15000
+monster.race = "blood"
+monster.corpse = 28737
+monster.speed = 110
+monster.manaCost = 0
+
+monster.changeTarget = {
+	interval = 4000,
+	chance = 10,
+}
+
+monster.strategiesTarget = {
+	nearest = 100,
+}
+
+monster.flags = {
+	summonable = false,
+	attackable = true,
+	hostile = true,
+	convinceable = false,
+	pushable = false,
+	rewardBoss = false,
+	illusionable = false,
+	canPushItems = true,
+	canPushCreatures = true,
+	staticAttackChance = 90,
+	targetDistance = 1,
+	runHealth = 0,
+	healthHidden = false,
+	isBlockable = false,
+	canWalkOnEnergy = true,
+	canWalkOnFire = true,
+	canWalkOnPoison = true,
+}
+
+monster.light = {
+	level = 0,
+	color = 0,
+}
+
+monster.voices = {
+	interval = 5000,
+	chance = 10,
+	{ text = "Mmmhaarrrgh!", yell = false },
+}
+
+monster.loot = {
+	{ name = "soul orb", chance = 35000 },
+	{ name = "great mana potion", chance = 33000, maxCount = 3 },
+	{ name = "great health potion", chance = 33000, maxCount = 3 },
+	{ name = "assassin star", chance = 24670, maxCount = 20 },
+	{ id = 282, chance = 13000 }, -- giant shimmering pearl (brown)
+	{ name = "falcon crest", chance = 21250 },
+	{ name = "war axe", chance = 12230 },
+	{ name = "violet gem", chance = 1060 },
+	{ name = "damaged armor plates", chance = 8990 },
+	{ name = "golden armor", chance = 4840 },
+	{ name = "mastermind shield", chance = 2620 },
+	{ name = "heavy mace", chance = 2460 },
+	{ id = 3019, chance = 18000 }, -- demonbone amulet
+	{ id = 28718, chance = 1000 },
+	{ id = 28716, chance = 1000 },
+	{ id = 28717, chance = 1000 },
+	{ id = 28719, chance = 1000 },
+	{ id = 28721, chance = 1000 },
+	{ id = 28715, chance = 1000 },
+}
+
+monster.attacks = {
+	{ name = "melee", interval = 2000, chance = 100, minDamage = 0, maxDamage = -600 },
+	{ name = "combat", interval = 2000, chance = 18, type = COMBAT_EARTHDAMAGE, minDamage = -400, maxDamage = -600, radius = 2, effect = CONST_ME_GROUNDSHAKER, target = false },
+	{ name = "combat", interval = 2000, chance = 14, type = COMBAT_HOLYDAMAGE, minDamage = -290, maxDamage = -460, length = 5, spread = 0, effect = CONST_ME_BLOCKHIT, target = false },
+	{ name = "combat", interval = 2000, chance = 50, type = COMBAT_PHYSICALDAMAGE, minDamage = -400, maxDamage = -650, range = 5, shootEffect = CONST_ANI_ROYALSPEAR, target = true },
+	{ name = "combat", interval = 2000, chance = 20, type = COMBAT_PHYSICALDAMAGE, minDamage = -200, maxDamage = 600, range = 5, shootEffect = CONST_ANI_BOLT, target = true },
+	{ name = "combat", interval = 2000, chance = 15, type = COMBAT_ENERGYDAMAGE, minDamage = -250, maxDamage = -650, range = 7, radius = 2, shootEffect = CONST_ANI_POWERBOLT, effect = CONST_ME_ENERGYHIT, target = true },
+	{ name = "combat", interval = 2000, chance = 10, type = COMBAT_ENERGYDAMAGE, minDamage = -250, maxDamage = -650, length = 5, spread = 0, effect = CONST_ME_PURPLEENERGY, target = false },
+
+
+}
+
+monster.defenses = {
+	defense = 86,
+	armor = 86,
+	mitigation = 3.50,
+}
+
+monster.elements = {
+	{ type = COMBAT_PHYSICALDAMAGE, percent = 20 },
+	{ type = COMBAT_ENERGYDAMAGE, percent = 0 },
+	{ type = COMBAT_EARTHDAMAGE, percent = 0 },
+	{ type = COMBAT_FIREDAMAGE, percent = -10 },
+	{ type = COMBAT_LIFEDRAIN, percent = 0 },
+	{ type = COMBAT_MANADRAIN, percent = 0 },
+	{ type = COMBAT_DROWNDAMAGE, percent = 0 },
+	{ type = COMBAT_ICEDAMAGE, percent = -10 },
+	{ type = COMBAT_HOLYDAMAGE, percent = 0 },
+	{ type = COMBAT_DEATHDAMAGE, percent = 50 },
+}
+
+monster.immunities = {
+	{ type = "paralyze", condition = true },
+	{ type = "outfit", condition = false },
+	{ type = "invisible", condition = true },
+	{ type = "bleed", condition = false },
+}
+
+mType:register(monster)
