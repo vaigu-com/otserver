ba7b063c37e2e97af02ba567705fcfd3ed902f4c
un000000 <bolter156@gmail.com>
2025-05-18 14:54:48 +0200

    fix: werecreature bosses fixes

--- data-vaigu/scripts/creature_event/on_death_monster/wereboss_death.lua
+++ data-vaigu/scripts/creature_event/on_death_monster/wereboss_death.lua
@@ -1,8 +1,18 @@
+local wereBossDeathStartup = GlobalEvent("WereBossDeathStartup")
+function wereBossDeathStartup.onStartup()
+	for _, bossData in pairs(WEREBOSS_DATA) do
+		local monsterType = MonsterType(bossData.bossName)
+		if monsterType then
+			monsterType:registerEvent("WereBossDeath")
+		end
+	end
+end
+wereBossDeathStartup:register()
+
 local wereBossDeath = CreatureEvent("WereBossDeath")
 function wereBossDeath.onDeath(creature)
 	onDeathForDamagingPlayers(creature, function(creature, player)
 		player:setStorageValueByKey(Storage.WereBossKill, os.time() + 6 * 60 * 60)
 	end)
 end
-
 wereBossDeath:register()
