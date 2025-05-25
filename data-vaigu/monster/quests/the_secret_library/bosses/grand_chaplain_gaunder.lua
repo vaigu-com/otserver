bbb8e2f59c82f4f558e62bd3a2fa68f3be64b44f
Kelixa-Vaigu <denisdenis666@gmail.com>
2025-05-25 18:51:14 +0200

    oberon bosses loot

--- data-vaigu/monster/quests/the_secret_library/bosses/grand_chaplain_gaunder.lua
+++ data-vaigu/monster/quests/the_secret_library/bosses/grand_chaplain_gaunder.lua
@@ -18,8 +18,8 @@ monster.bosstiary = {
 	bossRace = RARITY_BANE,
 }
 
-monster.health = 18000
-monster.maxHealth = 18000
+monster.health = 11000
+monster.maxHealth = 11000
 monster.race = "blood"
 monster.corpse = 28733
 monster.speed = 105
@@ -71,28 +71,23 @@ monster.voices = {
 }
 
 monster.loot = {
-	{ name = "flask of demonic blood", chance = 50000, maxCount = 2 },
 	{ name = "ham", chance = 50000, maxCount = 2 },
-	{ name = "onyx arrow", chance = 35000, maxCount = 3 },
 	{ name = "small diamond", chance = 30000, maxCount = 3 },
 	{ name = "small emerald", chance = 30000, maxCount = 3 },
 	{ name = "small enchanted amethyst", chance = 20000, maxCount = 3 },
 	{ name = "damaged armor plates", chance = 2350, maxCount = 3 },
 	{ id = 281, chance = 12000, maxCount = 1 }, -- giant shimmering pearl (green)
-	{ name = "knight armor", chance = 7000 },
 	{ name = "patch of fine cloth", chance = 1800 },
-	{ name = "spiked squelcher", chance = 3200 },
-	{ name = "titan axe", chance = 2400 },
-	{ name = "falcon battleaxe", chance = 200 },
-	{ name = "falcon longsword", chance = 200 },
-	{ name = "falcon mace", chance = 210 },
-	{ name = "falcon plate", chance = 100 },
-	{ name = "falcon shield", chance = 100 },
+	{ name = "falcon battleaxe", chance = 500 },
+	{ name = "falcon longsword", chance = 500 },
+	{ name = "falcon mace", chance = 500 },
+	{ name = "falcon plate", chance = 200 },
+	{ name = "falcon shield", chance = 200 },
 }
 
 monster.attacks = {
-	{ name = "melee", interval = 2000, chance = 100, minDamage = -150, maxDamage = -850 },
-	{ name = "combat", interval = 1500, chance = 15, type = COMBAT_LIFEDRAIN, minDamage = -380, maxDamage = -890, range = 4, radius = 4, effect = CONST_ME_HITBYPOISON, target = false },
+	{ name = "melee", interval = 2000, chance = 100, minDamage = -150, maxDamage = -750 },
+	{ name = "combat", interval = 1500, chance = 15, type = COMBAT_LIFEDRAIN, minDamage = -380, maxDamage = -790, range = 4, radius = 4, effect = CONST_ME_HITBYPOISON, target = false },
 	{ name = "combat", interval = 2000, chance = 20, type = COMBAT_PHYSICALDAMAGE, minDamage = -290, maxDamage = -720, range = 7, shootEffect = CONST_ANI_ETHEREALSPEAR, target = false },
 	{ name = "combat", interval = 1500, chance = 14, type = COMBAT_HOLYDAMAGE, minDamage = -300, maxDamage = -1000, length = 8, spread = 0, effect = CONST_ME_BLOCKHIT, target = false },
 	{ name = "combat", interval = 1000, chance = 15, type = COMBAT_LIFEDRAIN, minDamage = -200, maxDamage = -700, range = 5, radius = 3, effect = CONST_ME_SMALLCLOUDS, target = false },
@@ -107,15 +102,15 @@ monster.defenses = {
 }
 
 monster.elements = {
-	{ type = COMBAT_PHYSICALDAMAGE, percent = 80 },
+	{ type = COMBAT_PHYSICALDAMAGE, percent = 60 },
 	{ type = COMBAT_ENERGYDAMAGE, percent = 50 },
 	{ type = COMBAT_EARTHDAMAGE, percent = 50 },
 	{ type = COMBAT_FIREDAMAGE, percent = 50 },
 	{ type = COMBAT_LIFEDRAIN, percent = 0 },
 	{ type = COMBAT_MANADRAIN, percent = 0 },
 	{ type = COMBAT_DROWNDAMAGE, percent = 0 },
-	{ type = COMBAT_ICEDAMAGE, percent = 50 },
-	{ type = COMBAT_HOLYDAMAGE, percent = 0 },
+	{ type = COMBAT_ICEDAMAGE, percent = -20 },
+	{ type = COMBAT_HOLYDAMAGE, percent = 80 },
 	{ type = COMBAT_DEATHDAMAGE, percent = 50 },
 }
 
