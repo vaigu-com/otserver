83ed4ff6aae78a0b5a2ff95060aef2ef2995bceb
un000000 <bolter156@gmail.com>
2025-05-26 23:28:25 +0200

    fix: adjust corrupt naga loot

--- data-vaigu/monster/reptiles/corrupt_naga.lua
+++ data-vaigu/monster/reptiles/corrupt_naga.lua
@@ -60,11 +60,11 @@ monster.voices = {
 }
 
 monster.loot = {
+	{ name = "platinum coin", chance = 100000, maxCount = 17 },
 	{ id = 39415, chance = 15540 },
 	{ id = 11688, chance = 330 },
 	{ id = 11693, chance = 330 },
 	{ id = 11673, chance = 5330, maxCount = 2 },
-	{ id = 22516, chance = 500 },
 	{ id = 39411, chance = 5960, maxCount = 2 },
 	{ id = 39412, chance = 5960, maxCount = 2 },
 }
