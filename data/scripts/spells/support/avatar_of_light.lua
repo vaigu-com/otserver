a2012db220a6285ce5d4caa4fa0adcf4a031bccb
un000000 <bolter156@gmail.com>
2025-05-26 19:52:01 +0200

    fix: set wheel spells required level to 50

--- data/scripts/spells/support/avatar_of_light.lua
+++ data/scripts/spells/support/avatar_of_light.lua
@@ -29,7 +29,7 @@ spell:group("support")
 spell:id(265)
 spell:name("Avatar of Light")
 spell:words("uteta res sac")
-spell:level(300)
+spell:level(50)
 spell:mana(1500)
 spell:isPremium(true)
 spell:cooldown(2 * 60 * 60 * 1000) -- Default cooldown = 2 hours
