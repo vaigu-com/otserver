eee686fc93e9fd3bcdca2fd38af97ed31b63a201
un000000 <bolter156@gmail.com>
2025-05-28 21:14:10 +0200

    dont save map on moving house items to depot

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -10915,7 +10915,6 @@ void Game::transferHouseItemsToDepot() {
 	if (transferSuccess > 0) {
 		g_logger().info("Finished house transfer items from '{}' players", transferSuccess);
 		transferHouseItemsToPlayer.clear();
-		Map::save();
 	}
 }
 
