2b1aa2009b35916d2f2828ff983531b4b46fea8d
un000000 <bolter156@gmail.com>
2025-06-07 06:41:11 +0200

    refactor: empty flask talkaction

--- data/scripts/talkactions/player/flask.lua
+++ data/scripts/talkactions/player/flask.lua
@@ -1,22 +1,42 @@
-local flask = TalkAction("!flask")
+local flaskDiscardStorage = "talkaction.potions.flask"
 
-function flask.onSay(player, words, param)
-	if param == "" then
-		player:sendCancelMessage("You need to specify on/off param.")
-		return true
-	end
-	if param == "on" then
-		player:kv():set("talkaction.potions.flask", true)
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You will not receive flasks!")
-		player:getPosition():sendMagicEffect(CONST_ME_REDSMOKE)
-	elseif param == "off" then
-		player:kv():remove("talkaction.potions.flask")
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You will receive flasks.")
-		player:getPosition():sendMagicEffect(CONST_ME_REDSMOKE)
-	end
-	return true
+local dontReceiveFlasks = true
+local doReceiveFlasks = nil
+
+function Player:ShouldReceiveEmptyPotionFlasks()
+	return self:getStorageValueByKey(flaskDiscardStorage) == doReceiveFlasks
 end
 
+local function setNotReceive(player)
+	player:setStorageValueByKey(flaskDiscardStorage, dontReceiveFlasks)
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You will not receive flasks!")
+	player:getPosition():sendMagicEffect(CONST_ME_REDSMOKE)
+end
+
+local function setYesReceive(player)
+	player:removeStorageValueByKey(flaskDiscardStorage)
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You will receive flasks.")
+	player:getPosition():sendMagicEffect(CONST_ME_REDSMOKE)
+end
+
+local flask = TalkAction("!flask", "!vial", "/flask", "/vial")
+function flask.onSay(player, words, param)
+	if param == "on" then
+		setNotReceive(player)
+	elseif param == "off" then
+		setYesReceive(player)
+	elseif param == "" then
+		if player:ShouldReceiveEmptyPotionFlasks() then
+			setNotReceive(player)
+		else
+			setYesReceive(player)
+		end
+	else
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Usage: /flask [on|off]")
+	end
+
+	return true
+end
 flask:separator(" ")
 flask:groupType("normal")
 flask:register()
