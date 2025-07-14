6254deafe73578e4995d54f1a9379fed3e39f43d
un000000 <bolter156@gmail.com>
2025-07-14 02:53:01 +0200

    fix: build

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -2083,7 +2083,7 @@ void Player::sendPing() {
 	if (noPongTime >= 60000 && shouldForceLogout) {
 		if (canLogout() && g_creatureEvents().playerLogout(static_self_cast<Player>())) {
 			g_logger().info("Player {} has been kicked due to ping timeout. (has client: {})", getName(), client != nullptr);
-			player->setLoggingOut(true);
+			setLoggingOut(true);
 			if (client) {
 				client->logout(true, true);
 			} else {
