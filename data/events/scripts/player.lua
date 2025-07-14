97e9a6895848bb70cfb7d74407b324655fa6a66d
un000000 <bolter156@gmail.com>
2025-07-15 01:40:10 +0200

    fix: allow moving and unwraping store items

--- data/events/scripts/player.lua
+++ data/events/scripts/player.lua
@@ -278,6 +278,7 @@ do
 	ImmovableKeys:Add(IMMOVABLE_KEY)
 end
 
+local zStackTop = 255
 local function isImmovable(item, fromPosition, toPosition)
 	if immovableAid[item:getActionId()] then
 		return true
@@ -286,30 +287,38 @@ local function isImmovable(item, fromPosition, toPosition)
 	if ImmovableKeys:Has(item:getKey()) then
 		return true
 	end
-	return false
-end
 
-local exhaust = {}
-function Player:onMoveItem(item, count, fromPosition, toPosition, fromCylinder, toCylinder)
-	if isImmovable(item, fromPosition, toPosition) then
-		self:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+	if item:getCustomAttribute(HOUSE_DECORATION_STATUS) == IS_HOUSE_DECORATION then 
 		return false
 	end
 
 	if isInStoreinbox(item) then
 		--Dont allow moving items from storeinbox inner containers to storeinbox main container
 		if toPosition.y ~= fromPosition.y then
-			return false
+			return true
 		end
 		--Dont allow moving items from storeinbox main container to inner storeinbox containers
-		if toPosition.z ~= 255 then
-			return false
+		if toPosition.z ~= zStackTop then
+			return true
 		end
 	else
 		local key = item:getKey()
 		if key and key ~= "" then
-			return false
+			return true
 		end
+	end
+
+	return false
+end
+
+HOUSE_DECORATION_STATUS = "HOUSE_DECORATION_STATUS"
+IS_HOUSE_DECORATION = true
+
+local exhaust = {}
+function Player:onMoveItem(item, count, fromPosition, toPosition, fromCylinder, toCylinder)
+	if isImmovable(item, fromPosition, toPosition) then
+		self:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+		return false
 	end
 
 	-- No move if item count > 30 items
