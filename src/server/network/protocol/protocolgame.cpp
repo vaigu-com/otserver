95aa21e40fe0808a5d144ec80558a17bf4dacd7c
un000000 <bolter156@gmail.com>
2025-08-04 22:48:07 +0200

    improve: avoid false positive error message

--- src/server/network/protocol/protocolgame.cpp
+++ src/server/network/protocol/protocolgame.cpp
@@ -729,7 +729,7 @@ void ProtocolGame::login(const std::string &name, uint32_t accountId, OperatingS
 		player->loginProtectionTime = OTSYS_TIME() + g_configManager().getNumber(LOGIN_PROTECTION_TIME);
 		acceptPackets = true;
 	} else {
-		if (eventConnect != 0 || !g_configManager().getBoolean(REPLACE_KICK_ON_LOGIN)) {
+		if (!g_configManager().getBoolean(REPLACE_KICK_ON_LOGIN)) {
 			// Already trying to connect
 			disconnectClient("You are already logged in.");
 			return;
