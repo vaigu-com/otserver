631e802b81359dc3657cda222b7ff78ca2cbbc05
un000000 <bolter156@gmail.com>
2025-08-05 15:16:47 +0200

    fix: correctly store and display server uptime

--- data/scripts/talkactions/misc/uptime.lua
+++ data/scripts/talkactions/misc/uptime.lua
@@ -1,11 +1,19 @@
+Game.setStorageValueByKey(Storage.LastStartupTimestamp, os.time())
+
 local talkaction = TalkAction("!uptime")
-
 function talkaction.onSay(player, words, param)
-	local uptime = (os.time() - Game.getStorageValueByKey(Storage.LastStartupTimestamp))
+	local now = os.time()
+	local lastSave = Game.getStorageValueByKey(Storage.LastStartupTimestamp)
 
+	local uptime = math.abs(now - lastSave)
+
+	local days = math.floor(uptime / 86400)
+	uptime = uptime % 86400
 	local hours = math.floor(uptime / 3600)
-	local minutes = math.floor((uptime - (3600 * hours)) / 60)
-	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Uptime: " .. hours .. " hours i " .. minutes .. " ,omites.")
+	uptime = uptime % 3600
+	local minutes = math.floor(uptime / 60)
+
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, T("Uptime: :d: days :h: hours :m: minutes.\n Last save: :lastSave:\nNow: :now:", { d = days, h = hours, m = minutes, lastSave = lastSave, now = now }))
 	return true
 end
 
