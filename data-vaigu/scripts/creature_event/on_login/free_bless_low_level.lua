674a4fdae720168b78a8ffdbd8eb3bdca3848c43
un000000 <bolter156@gmail.com>
2025-09-19 00:15:35 +0200

    fix: correct free bless levels

--- data-vaigu/scripts/creature_event/on_login/free_bless_low_level.lua
+++ data-vaigu/scripts/creature_event/on_login/free_bless_low_level.lua
@@ -1,18 +1,8 @@
-function GrantFreeBlessLowLevel(player)
-	if player:getLevel() > MAX_LVL_TO_GET_FREE_BLESS then
-		return
-	end
-
-	for i = 2, 6 do
-		if not player:hasBlessing(i) then
-			player:addBlessing(i, 1)
-		end
-	end
-
+local function notifyPlayerCanGetFreeBless(player)
 	if getPlayerLanguage(player) == "PL" then
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Do " .. MAX_LVL_TO_GET_FREE_BLESS .. " poziomu mozesz otrzymac darmowe blogoslawienstwa. Zglos sie do Ksiedza Natanka, a cie poblogoslawi.")
 	else
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You can receive free blessings until " .. MAX_LVL_TO_GET_FREE_BLESS .. " level. Ask Father Natanek for help.")
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You can receive free blessings until " .. MAX_LVL_TO_GET_FREE_BLESS .. " level. Ask Father Natanek and we will surely help you.")
 	end
 	player:getPosition():sendMagicEffect(CONST_ME_HOLYDAMAGE)
 	return LOGIN_OK
@@ -20,7 +10,10 @@ end
 
 local creatureevent = CreatureEvent("freebless")
 function creatureevent.onLogin(player)
-	GrantFreeBlessLowLevel(player)
+	if player:getLevel() > MAX_LVL_TO_GET_FREE_BLESS then
+		return
+	end
+	notifyPlayerCanGetFreeBless(player)
 	return LOGIN_OK
 end
 creatureevent:register()
