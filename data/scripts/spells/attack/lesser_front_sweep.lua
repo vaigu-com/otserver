f0357b69198b1ef4f248156e5c1a57a6300c0808
un000000 <bolter156@gmail.com>
2025-10-03 00:14:34 +0200

    fix: spell name

--- data/scripts/spells/attack/lesser_front_sweep.lua
+++ data/scripts/spells/attack/lesser_front_sweep.lua
@@ -20,7 +20,7 @@ function spell.onCastSpell(creature, var)
 end
 
 spell:group("attack")
-spell:name("Front Sweep")
+spell:name("Lesser Front Sweep")
 spell:words("exori infir min")
 spell:castSound(SOUND_EFFECT_TYPE_SPELL_FRONT_SWEEP)
 spell:level(1)
